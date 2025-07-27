803553c35d85fcf03121e8c1545b49db39769946
anujsolanki716 <anujsolanki716@gmail.com>
2025-07-27 19:39:13 +0530

    add express

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import eventRoutes from './routes/eventRoutes.js';
 import aiRoutes from './routes/aiRoutes.js';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
+// const helmet = require('helmet');
+// const rateLimit = require('express-rate-limit');
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+//  Middleware
+app.use(helmet()); // Secure headers
+app.use(cors({ origin: 'http://localhost:5173' })); // Allow frontend
+app.use(express.json());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
 
 app.use(cors());
 app.use(express.json());
